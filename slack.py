37f253789adbc016aab37027199217db3c05c9ce
goldsag <goldsagi2@gmail.com>
2023-02-13 15:36:26 +0200

    slack url

--- slack.py
+++ slack.py
@@ -1,7 +1,7 @@
 
 from slack_sdk.webhook import WebhookClient
 
-url = "https://hooks.slack.com/services/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"
+url = "https://hooks.slack.com/services/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"
 webhook = WebhookClient(url)
 
 def sendSlackMessage (status):
